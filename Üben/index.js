b38f1ece4b6cdcda10ed5e57dcac9aefbbb0a95a
Josi3103 <josiasdeininger@outlook.de>
2025-05-07 18:15:55 +0200

    Add files via upload

---
+++ Üben/index.js
@@ -0,0 +1,7 @@
+function eingabe1(){
+    const content = document.getElementById("hiddenContent");
+    content.style.display = "block";
+}
+function eingabe2(){
+    document.getElementById("hiddenContent").style.display = "block";
+}
